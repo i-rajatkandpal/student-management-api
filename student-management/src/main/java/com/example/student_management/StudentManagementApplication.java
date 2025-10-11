279495f4b45c30049d5b3356cd248476e7ea2cd9
i-rajatkandpal <115024147+i-rajatkandpal@users.noreply.github.com>
2025-10-12 00:28:25 +0530

    Endpoint & connections

---
+++ student-management/src/main/java/com/example/student_management/StudentManagementApplication.java
@@ -0,0 +1,15 @@
+package com.example.student_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.TimeZone;
+
+@SpringBootApplication
+public class StudentManagementApplication {
+
+	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+		SpringApplication.run(StudentManagementApplication.class, args);
+	}
+}
